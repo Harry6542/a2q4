e766868dbee7b29d3fbf8fdace3db92a9e65551d
Harry6542 <ozc189@usak.ca>
2023-06-18 12:05:56 -0600

    Started the program

---
+++ A2q2.py
@@ -0,0 +1,17 @@
+###############################################################################
+# CMPT 145 Course material
+# Original Author: Lauresa Stilling
+# Date Created:   31 May 2023
+# Last Edited:    31 May 2023
+#
+# All rights reserved.
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this
+# file to a public or private website, or providing this file to a person
+# not registered in CMPT 145, constitutes Academic Misconduct, according
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+# Synopsis:
+#    Testing; relevant to Chapter 5, 6, 7
+###############################################################################
+
