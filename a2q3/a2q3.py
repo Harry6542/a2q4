666089223be76b8d614c900efc1ee6fe8e04779c
Harry6542 <ozc189@usak.ca>
2023-06-18 13:27:29 -0600

    def copy_list_of_lists()

---
+++ a2q3/a2q3.py
@@ -0,0 +1,27 @@
+# CMPT 145 Course material
+# Original Author: Lauresa Stilling
+# Date Created:   31 May 2023
+# Last Edited:    31 May 2023
+#
+# All rights reserved.
+#
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this
+# file to a public or private website, or providing this file to a person
+# not registered in CMPT 145, constitutes Academic Misconduct, according
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+# Synopsis:
+#    Debugging exercise, relevant to Chapter 3 and 7
+###############################################################################
+
+# TODO: Fill in your information below
+# Student Name-Harry Patel
+# NSID-ozc189
+# Student Number-1135887
+
+# Recall another name for a list of dictionaries is a record.
+import copy
+
+
+def copy_list_of_lists(data: list) -> list:
