ac822d15520589c28c7338e91be018e21efb0637
Harry6542 <134742927+Harry6542@users.noreply.github.com>
2023-06-18 14:42:52 -0600

    Add files via upload

---
+++ a2q4.py
@@ -0,0 +1,25 @@
+###############################################################################
+# CMPT 145 Course material
+# Original Author: Lauresa Stilling
+# Date Created:   31 May 2023
+# Last Edited:    15 June 2023
+#
+# All rights reserved.
+#
+# This document contains resources for homework assigned to students of
+# CMPT 145 and shall not be distributed without permission.  Posting this
+# file to a public or private website, or providing this file to a person
+# not registered in CMPT 145, constitutes Academic Misconduct, according
+# to the University of Saskatchewan Policy on Academic Misconduct.
+#
+# Synopsis:
+#    Testing; relevant to Chapter 5, 6, 7
+###############################################################################
+
+# TODO: Fill in your information below
+# Student Name-Harry Patel
+# NSID-ozc189
+# Student Number-11358887
+
+# TODO: TEST DRIVEN DEVELOPMENT
+
